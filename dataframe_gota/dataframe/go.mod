d58ed2109d6051bed0a455c4d91ae3dde7a1a38e
carlosm27 <c_marcano@correo.unimet.edu.ve>
2021-12-27 20:15:09 -0400

    adding dataframe_gota

---
+++ dataframe_gota/dataframe/go.mod
@@ -0,0 +1,5 @@
+module dataframe_gota/dataframe
+
+go 1.16
+
+require github.com/go-gota/gota v0.12.0 // indirect
---
+++ dataframe_gota/dataframe_gota.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"strings"
+	"github.com/go-gota/gota"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the name of a file with .csv extension or '0.csv' to end the program: ")
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+
+	CsvName := strings.TrimSuffix(input,"\r\n")
+	if strings.HasSuffix(CsvName, ".csv") {
+		OpenCsvFile(CsvName)
+	}
+	fmt.Println("Enter the name of a file with .csv extension or anything else to end the program: ")
+
+	for strings.HasSuffix(CsvName, ".csv") {
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			return
+		}
+		CsvName := strings.TrimSuffix(input,"\r\n")
+		OpenCsvFile(CsvName)
+		fmt.Println("Enter the name of a file with .csv extension or anything else to end the program: ")
+	}
+	fmt.Println("Bye!")
+
+
+}
+func OpenCsvFile(name string) {
+    CsvFile, err := os.Open(name)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    df := dataframe.ReadCsv(CsvFile)
+    fmt.Println(df)
+}
